26c02b40edf6d84c4c78ca945f113ead0ccfe99d
radhikaprabu <radhikaprabu91699@gmail.com>
2017-04-24 15:41:28 -0500

    Created SQLite database

---
+++ ToDoApp/ToDoApp/CreateToDoDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ToDoApp
+{
+    public class CreateToDoDatabase
+    {
+        SQLiteAsyncConnection database;
+        public CreateToDoDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<ToDoItem>().Wait();
+        }
+        
+        public Task<List<ToDoItem>> GetItemsAsync()
+        {
+            return database.Table<ToDoItem>().ToListAsync();
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ToDoApp
+{
+    public class ToDoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ItemID { get; set; }
+
+        public string Item { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
